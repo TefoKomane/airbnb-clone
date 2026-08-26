383e5896c65ad8334e0d9abc419686dfc3e33be3
Tefo Karabo Komane <karabokomane29@gmail.com>
2026-08-26 09:06:04 +0000

    feat: add experiences section static data

---
+++ client/src/data/experiencesData.js
@@ -0,0 +1,17 @@
+// Static content for the "Discover Airbnb Experiences" section
+const experiencesData = [
+  {
+    id: 1,
+    title: "Things to do on your trip",
+    buttonLabel: "Experiences",
+    color: "#7c3a1d",
+  },
+  {
+    id: 2,
+    title: "Things to do from home",
+    buttonLabel: "Online Experiences",
+    color: "#374151",
+  },
+];
+
+export default experiencesData;
