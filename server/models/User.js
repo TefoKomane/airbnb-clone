38ad87ebb7cdfa99ace426bf58a1f70a66611b58
Tefo Karabo Komane <karabokomane29@gmail.com>
2026-08-26 09:06:03 +0000

    feat: complete User model with password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -25,3 +25,26 @@ const userSchema = new mongoose.Schema(
       type: String,
       enum: ["user", "host"],
       default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+// hash the password automatically whenever it is created or changed
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+    return;
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// helper method used during login to compare the typed password
+// against the hashed one stored in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
