c2a1f18350e383fcdc4ccf06b4552d6f97acaeca
Tefo Karabo Komane <karabokomane29@gmail.com>
2026-08-26 09:06:03 +0000

    feat: start MongoDB connection config

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+// Handles the connection to MongoDB using Mongoose.
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
