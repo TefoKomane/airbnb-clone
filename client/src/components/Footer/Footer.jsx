3ea8a0817166525706efd40b4b280d9f36c21570
Tefo Karabo Komane <karabokomane29@gmail.com>
2026-08-26 09:06:04 +0000

    feat: scaffold site footer with link columns

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import Icon from "../Icon/Icon.jsx";
+import "./Footer.css";
+
+// static footer matching the four column layout from the design brief
+const linkColumns = [
+  {
+    heading: "Support",
+    links: [
+      "Help Center",
+      "Safety information",
+      "Cancellation options",
+      "Our COVID-19 Response",
+      "Supporting people with disabilities",
+      "Report a neighborhood concern",
+    ],
+  },
+  {
+    heading: "Community",
+    links: [
+      "Airbnb.org: disaster relief housing",
+      "Support: Afghan refugees",
+      "Celebrating diversity & belonging",
+      "Combating discrimination",
+    ],
+  },
+  {
