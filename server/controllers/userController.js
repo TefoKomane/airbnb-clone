a7286c8993314165fb3dbd3a0905cf57e7240e99
Tefo Karabo Komane <karabokomane29@gmail.com>
2026-08-26 09:06:03 +0000

    feat: build out user controller: register, login, getMe

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,3 +27,32 @@ const registerUser = async (req, res, next) => {
     });
 
     res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id, user.role),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @desc    Log a user in
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("Please provide an email and password");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        username: user.username,
