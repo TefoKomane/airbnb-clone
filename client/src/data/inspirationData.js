1eced52159c6cc591f8c2dabe06a8c3815ca54c4
Tefo Karabo Komane <karabokomane29@gmail.com>
2026-08-26 09:06:04 +0000

    feat: add inspiration section static data

---
+++ client/src/data/inspirationData.js
@@ -0,0 +1,29 @@
+// Static content for the "Inspiration for your next trip" section on the home page
+const inspirationData = [
+  {
+    id: 1,
+    title: "Sandton City Hotel",
+    subtitle: "53 km away",
+    color: "#7b1e3d",
+  },
+  {
+    id: 2,
+    title: "Joburg City Hotel",
+    subtitle: "168 km away",
+    color: "#a11d5c",
+  },
+  {
+    id: 3,
+    title: "Woodmead Hotel",
+    subtitle: "30 miles away",
+    color: "#c2410c",
+  },
+  {
+    id: 4,
+    title: "Hyde Park Hotel",
+    subtitle: "34 km away",
+    color: "#dc3d3d",
+  },
+];
+
+export default inspirationData;
